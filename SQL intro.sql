eb40fbf17b3f37071897097001645b980ba3d35a
harshapy5 <87979310+harshapy5@users.noreply.github.com>
2023-10-09 22:03:22 +0530

    Add files via upload

---
+++ SQL intro.sql
@@ -0,0 +1,24 @@
+SHOW databases;
+CREATE DATABASE IF NOT EXISTS Bank_Details;
+USE Bank_Details;
+
+CREATE TABLE IF NOT EXISTS Bank_Details(
+age int,
+job varchar(30),
+marital varchar(30),
+education varchar(30),
+`default` varchar(30),
+balance int,
+housing varchar(30),
+loan varchar(30),
+contact varchar(30),
+`day` int,
+`month` varchar(30),
+duration int,
+campaign int,
+pdays int,
+previous int,
+poutcome varchar(30),
+y varchar(30))
+
+SELECT * FROM Bank_Details;
