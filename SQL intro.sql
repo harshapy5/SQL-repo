f8e8574b471e3d3f760bb677c27b3df5829eea42
harshapy5 <87979310+harshapy5@users.noreply.github.com>
2023-10-10 21:44:57 +0530

    Update SQL intro.sql

--- SQL intro.sql
+++ SQL intro.sql
@@ -1,7 +1,7 @@
 -- This file contains basic SQL code on how to create a database and use the database to create table and insert data into the table.
 
 -- SQL query to create a database
-CREATE DATABASE IF NOT EXISTS Bank_Details;
+CREATE DATABASE IF NOT EXISTS Dress_details;
 
 -- SQL Query to sow the available databases in the DBMS
 SHOW databases;
@@ -12,18 +12,18 @@ USE <Database_name>;
 -- SQL Query to create a table if not existing in database
 CREATE TABLE IF NOT EXISTS <Table_name>(<Col1, Col2, Col3,etc>)
 
--- For our SQL practice I am using a excel file as a source which is taken from a notable website. Link : https://archive.ics.uci.edu/
+-- For our SQL practice I am using a excel file as a source which is taken from a notable website.
 
-CREATE TABLE IF NOT EXISTS Bank_details(
-age int, job varchar(30), marital varchar(30), education varchar(30), `default` varchar(30), balance int, housing varchar(30),loan varchar(30),
-contact varchar(30), `day` int, `month` varchar(30), duration int, campaign int, pdays int, previous int,poutcome varchar(30), y varchar(30))
+CREATE TABLE IF NOT EXISTS Dress_data(
+`Dress_ID` varchar(30),	`Style`	varchar(30),	`Price`	varchar(30),	 `Rating`	varchar(30),	`Size`	varchar(30),	`Season`	varchar(30),	
+`NeckLine`	varchar(30),	`SleeveLength` varchar(30),		`waiseline`	varchar(30),	`Material`	varchar(30),	`FabricType`	varchar(30),	
+`Decoration`	varchar(30),	`Pattern Type` varchar(30),		`Recommendation` varchar(30));
 
 -- SQL Query to select all the data from the table
 SELECT * FROM <Table_name>;
 
-SELECT * FROM Bank_Details;
+SELECT * FROM Dress_date;
 
 -- SQL Query to manually insert the data to the table
 INSERT INTO <Table_name> VALUES (Val1, Val2, Val3,etc)
 
-INSERT INTO bank_details VALUES(58,"management","married","tertiary","no",2143,"yes","no","unknown",5,"may",261,1,-1,0,"unknown","no")
